264b5bae02cde68f18003b8b0363dce93c2ece2b
Ray Joshua <joshuaalgenio@gmail.com>
2025-08-14 17:41:44 +0800

    Add project files.

---
+++ Selenium-Automation/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumAutomation
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void ExampleTest()
+        {
+            driver.Navigate().GoToUrl("https://example.com");
+            Assert.AreEqual("Example", driver.Title);
+            Console.WriteLine("Test Passed - Title matched");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
